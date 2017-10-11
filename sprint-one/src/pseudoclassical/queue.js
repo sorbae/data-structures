71da44c10f7c7e031351aab6714b2632bfc325bd
Hack Reactor Student <student@hackreactor.com>
2017-10-11 16:41:52 -0700

    added code for queue and stack

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,21 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.currentIndex = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var disQ = this.storage[this.currentIndex + 1];
+  delete this.storage[this.currentIndex + 1];
+  this.currentIndex++;
+  return disQ;
+};
+
+Queue.prototype.size = function() {
+  return Math.max(0, this.count - this.currentIndex);
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,22 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
 };
 
+Stack.prototype.size = function() {
+  return Math.max(0, this.count);
+};
 
+Stack.prototype.pop = function() {
+  var datPopDoe = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return datPopDoe;
+};
+
+Stack.prototype.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+var test = new Stack(); 
