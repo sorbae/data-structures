86c8b4283f0c5d96bef5c5e6e1ac0a656a06e2dd
Hack Reactor Student <student@hackreactor.com>
2017-10-11 15:36:09 -0700

    added code for stack to work

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,37 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.count = 0;
+  
+  extend(newStack, stackMethods);
+  
+  return newStack;
 };
 
 var stackMethods = {};
 
 
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
+
+stackMethods.size = function() {
+  return Math.max(0, this.count);
+};
+
+stackMethods.push = function(value) {
+  this.count++;
+  this.storage[this.count] = value;
+};
+
+stackMethods.pop = function() {
+  var popBack = this.storage[this.count];
+  delete this.storage[this.count];
+  this.count--;
+  return popBack;
+};
