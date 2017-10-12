e380de94c92e962ebe81376443ba61f4d5a47c80
Hack Reactor Student <student@hackreactor.com>
2017-10-11 22:22:00 -0700

    updated code to work with more than 2 nodes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,39 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = Node(null);
+  list.tail = Node(null);
 
-  list.addToTail = function(value) {
+  list.addToTail = function(value) { 
+    // declare variable for newNode instance
+    // set list.tail to point to newNode instance
+    var newNode = Node(value);
+    if (list.tail.next === null) {
+      list.tail.next = newNode;
+    } 
+    if (list.head.next === null) {
+      list.head = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    // debugger;
+    var currentHead = list.head;
+    list.head.next = currentHead.next;
+    list.head = currentHead.next;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    var isThere = false;
+    // iterate through list
+    // check node object for value prop
+    for (var key in list) {
+      if (list[key].value === target) {
+        isThere = true;
+      }
+    }
+    return isThere;
   };
 
   return list;
@@ -20,10 +44,15 @@ var Node = function(value) {
 
   node.value = value;
   node.next = null;
-
+  
   return node;
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var test = LinkedList();
+test.addToTail(4);
+test.addToTail(5);
+test.removeHead();
