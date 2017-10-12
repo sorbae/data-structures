3df40e40602bccd58e2387bdc87d3d7f3986f14e
Hack Reactor Student <student@hackreactor.com>
2017-10-12 10:49:41 -0700

    added code for tree sprint

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; 
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +12,36 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var newNode = Tree(value);
+  this.children.push(newNode);
 };
 
 treeMethods.contains = function(target) {
+  // check values in parent node
+  var isFound = false;
+  if (this.value === target) {
+    // if value exists, return true
+    isFound = true; 
+  } 
+  for (let node of this.children) {
+    if (node.contains(target)) {
+      isFound = true;
+    }
+  }
+
+  // if parent node has children, recurse
+  return isFound;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  the overall complexity of these functions will be O(n) due to the for loop 
+  inside of contains.
  */
+
+var testTree = Tree('hello');
+testTree.addChild('there');
+testTree.children[0].addChild('go');
+testTree.contains('go');
