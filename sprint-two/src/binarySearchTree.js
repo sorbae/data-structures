32c93838d37145d060fdee32a64e7d87d9367ba9
Hack Reactor Student <student@hackreactor.com>
2017-10-13 16:15:51 -0700

    added working code for bst sprint

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -9,46 +9,54 @@ let BinarySearchTree = function(value) {
 bstMethods = {};
 
 bstMethods.insert = function(value) {
-  console.log(this.binaryTree)
-  let node = this.binaryTree;
-  let nextLeaf;
-  if (value < node.value) {
-    nextLeaf = node.left;
-  }
-  if (value > node.value) {
-    nextLeaf = node.right;
-  }
-  // needs to check value against node's value
+  let nextLeaf = value < this.value ? this.left : this.right;
   
-  // if value > nodeVal && .right === null
-  
-  //   set .right = newLeaf
-  // else 
-  // set .left = newLeaf
-  
-  if (value > node.value && nextLeaf === null) { 
-    node.right = BinarySearchTree(value);
+  if (value > this.value && this.right === null) { 
+    this.right = BinarySearchTree(value);
+  } else if (value < this.value && this.left === null) {
+    this.left = BinarySearchTree(value);
+  } else {
+    nextLeaf.insert(value);
   }
-  if (value < node.value && nextLeaf === null) {
-    node.left = BinarySearchTree(value);
-  }
-  
-  nextLeaf.insert(value);
 };
 
-bstMethods.contains = function() {
-
+bstMethods.contains = function(target) {
+  let nextLeaf = target < this.value ? this.left : this.right;
+  // let wasFound = false;
+  // if this.value === target --> return true
+  // otherwise --> recurse(target)
+  if (this.value === target) {
+    return true;
+  }
+  if (this.left === null || this.right === null) {
+    return false;
+  }
+  return nextLeaf.contains(target);
 };
 
-bstMethods.depthFirstLog = function() {
-
+bstMethods.depthFirstLog = function(callback) {
+  callback(this.value);
+  
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+  
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+  // callback(this)
+  // this.left depthSearch 
+  // this.right depthSearch
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
  
-let treeTest = BinarySearchTree(5);
-treeTest.insert(2)
-console.log(treeTest)
+// let treeTest = BinarySearchTree(5);
+// treeTest.insert(2);
+// treeTest.insert(3);
+// treeTest.insert(7);
+// treeTest.insert(6);
+// console.log(treeTest.contains(7));
 
