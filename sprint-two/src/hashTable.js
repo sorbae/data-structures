27ea9935bafdb88b689715e7e84e60deb18f4104
Hack Reactor Student <student@hackreactor.com>
2017-10-13 11:32:41 -0700

    added working code for hash table

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,21 +6,63 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let storage = this._storage;
+  let inStorage = false;
+  debugger;
+  let lePairs = [k, v];
+  if (!storage.get(index)) {
+    storage.set(index, []);
+  }
+  let bucketArr = storage.get(index);
+  if (bucketArr.length > 0) {
+    for (let tuple of bucketArr) {
+      if (tuple[0] === k) {
+        inStorage = true;
+        tuple[1] = v;
+      }
+    }
+  }
+  if (!inStorage) {
+    bucketArr.push(lePairs);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let bucketArr = this._storage.get(index);
+  for (let tuple of bucketArr) {
+    if (tuple[0] === k) {
+      return tuple[1];
+    }
+  }
+  // return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+  let index = getIndexBelowMaxForKey(k, this._limit);
+  let bucketArr = this._storage.get(index);
+  // debugger;
+  if (bucketArr.length === 1) {
+    bucketArr.pop();
+  } else {
+    for (var i = 0; i < bucketArr.length; i++) {
+      if (bucketArr[i][0] === k) {
+        bucketArr.splice(i, 1);
+      }
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  its over 9000
  */
-
-
+ 
+ test = new HashTable();
+ test.insert('val1', 'val1');
+ test.insert('val2', 'val2');
+ test.insert('val3', 'val3');
+// test.remove('val2');
