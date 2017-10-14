00a0df5191f337ba5402a6fda4fe4f2ad024bd02
Hack Reactor Student <student@hackreactor.com>
2017-10-14 11:06:58 -0700

    added tests for duplicate arrays and objs. Added code to pass those test

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -21,17 +21,43 @@ describe('set', function() {
   it('should remove values from a set', function() {
     set.add('Mel Gibson');
     set.remove('Mel Gibson');
+    set.add([1, 2]);
+    set.remove([1, 2]);
+    
+    let checkInside = _.reduce(set._storage, function(contains, item) {
+      if (JSON.stringify([1, 2]) === JSON.stringify(item)) {
+        contains = true;
+      }
+      return contains;
+    }, false);
+    
     expect(set.contains('Mel Gibson')).to.equal(false);
+    expect(checkInside).to.equal(true);
   });
   
   it('should handle non-string types', function() {
     set.add(3);
     expect(set.contains(3)).to.equal(true);
     set.add([]);
-    expect(set.contains([])).to.equal(true);
+    expect(set.contains([])).to.equal(false);
     set.add({});
-    expect(set.contains({})).to.equal(true);
-  })
+    expect(set.contains({})).to.equal(false);
+  });
+  
+  it('should handle duplicate arrays and objects', function() {
+    var objAdd = {1: 2};
+    var dupObj = {1: 2};
+    var arrAdd = [1, 2];
+    var dupArr = [1, 2];
+    
+    set.add(objAdd);
+    set.add(dupObj);
+    set.add(arrAdd);
+    set.add(dupArr);
+
+    expect(set._storage).to.eql([{1: 2}, {1: 2}, [1, 2], [1, 2]]);
+    
+  });
   
   // write test for duplicate arrays & objects
 });
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,23 +1,35 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {}; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this._storage[item] = item;
+  if (typeof item === 'object' && item) {
+    this._storage.push(item);
+  } else {
+    if (!this.contains(item)) {
+      this._storage.push(item);
+    }
+  }
 };
 
 setPrototype.contains = function(item) {
-  return item in this._storage;
+  return this._storage.includes(item);
 };
 
 setPrototype.remove = function(item) {
-  delete this._storage[item];
+  if (this.contains(item)) {
+    this._storage.splice(this._storage.indexOf(item), 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ With our implementation of set, our overall average is O(n) since 
+ we store our values in an array inside of each Set. This requires
+ us to iterate on the array to find values when we add/remove from 
+ the set. 
  */
