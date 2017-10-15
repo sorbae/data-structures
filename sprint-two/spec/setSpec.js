319b53b9e307cc47e470694c379588c917de3106
Phoebe <meipbe@gmail.com>
2017-10-14 19:50:00 -0700

    Fixed set test file error

--- sprint-two/spec/setSpec.js
+++ sprint-two/spec/setSpec.js
@@ -23,18 +23,18 @@ describe('set', function() {
     set.remove('Mel Gibson');
     set.add([1, 2]);
     set.remove([1, 2]);
-    
+
     let checkInside = _.reduce(set._storage, function(contains, item) {
       if (JSON.stringify([1, 2]) === JSON.stringify(item)) {
         contains = true;
       }
       return contains;
     }, false);
-    
+
     expect(set.contains('Mel Gibson')).to.equal(false);
     expect(checkInside).to.equal(true);
   });
-  
+
   it('should handle non-string types', function() {
     set.add(3);
     expect(set.contains(3)).to.equal(true);
@@ -43,32 +43,29 @@ describe('set', function() {
     set.add({});
     expect(set.contains({})).to.equal(false);
   });
-  
+
   it('should handle duplicate arrays and objects', function() {
     var objAdd = {1: 2};
     var dupObj = {1: 2};
     var arrAdd = [1, 2];
     var dupArr = [1, 2];
-    
+
     set.add(objAdd);
     set.add(dupObj);
     set.add(arrAdd);
     set.add(dupArr);
 
     expect(set._storage).to.eql([{1: 2}, {1: 2}, [1, 2], [1, 2]]);
-    
+
   });
-  
-  it('should add two deeply equal arrays', function() {
-    set.add([1, 'dog', true]);
-    expect(set.size()).to.equal(2);
-  });
+
   it('should not add two strictly identical arrays', function() {
     var testArray = [1, 'dog', true];
     set.add(testArray);
+    set.add(testArray);
     expect(set.size()).to.equal(1);
   });
-    
+
   it('should be able to add functions', function() {
     var a = function(x) { return x; };
     var b = function(a, b, c) { a.push(b.concat(c)); };
@@ -79,7 +76,7 @@ describe('set', function() {
     expect(set.contains(b)).to.equal(true);
     expect(set.contains(Math.max)).to.equal(true);
   });
-  
+
   it('should be able to add arrays and objects', function() {
     var a = { 'a': true, '1234': 'bob'};
     var b = [4, 7, 'hello', false];
@@ -87,4 +84,5 @@ describe('set', function() {
     set.add(b);
     expect(set.contains(a)).to.equal(true);
     expect(set.contains(b)).to.equal(true);
+  });
 });
